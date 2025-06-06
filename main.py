564f39f2e991444d2b280e9a4a9e69424faf0bf0
jingfancai <jingfancai603@gmail.com>
2025-06-06 11:21:37 +0900

    by jfc

--- main.py
+++ main.py
@@ -16,3 +16,24 @@ class Complaint:
 
     def __str__(self):
         return f"[{self.complaint_date}] {self.author}：{self.content} (위치: {self.latitude}, {self.longitude})"
+
+st.set_page_config(page_title="동네 민원 신고 플랫폼", layout="wide")
+st.title("동네 민원/불편사항 신고 플랫폼")
+
+@st.cache_resource
+def get_gsheet_client():
+    flow = InstalledAppFlow.from_client_secrets_file(
+    'credentials1.json',
+    scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    creds = flow.run_local_server(port=8080)
+    client = gspread.authorize(creds)
+    return client
+
+def get_or_create_sheet():
+    client = get_gsheet_client()
+    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1NE0CYC_FgkSN6ankt9-x3uRzBSXWC52kHYc9qIZIp5Q/edit").sheet1
+    if sheet.row_count == 0 or sheet.get_all_values() == []:
+        sheet.append_row(["작성자", "내용", "위도", "경도", "날짜"])
+    elif sheet.row_values(1) != ["작성자", "내용", "위도", "경도", "날짜"]:
+        sheet.insert_row(["작성자", "내용", "위도", "경도", "날짜"], 1)
+    return sheet
